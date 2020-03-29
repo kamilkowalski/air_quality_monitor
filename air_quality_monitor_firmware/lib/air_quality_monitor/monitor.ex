948e307014a86e98e4a962a3c6b30477bbb4a4ae
Michal Korulczyk <michal.k@shedul.com>
2020-03-29 11:21:18 +0200

    Refactor: parse frames with circuits_uart_framing_pms7003

--- air_quality_monitor_firmware/lib/air_quality_monitor/monitor.ex
+++ air_quality_monitor_firmware/lib/air_quality_monitor/monitor.ex
@@ -4,6 +4,8 @@ defmodule AirQualityMonitor.Monitor do
 
   @baud_rate 9600
   @port_name "ttyAMA0"
+  @measurement_frequency 5000
+  @measurement_timeout 5000
 
   def start_link(arg) do
     GenServer.start_link(__MODULE__, arg)
@@ -12,37 +14,29 @@ defmodule AirQualityMonitor.Monitor do
   @impl true
   def init(_arg) do
     {:ok, uart_pid} = Circuits.UART.start_link()
-    :ok = Circuits.UART.open(uart_pid, @port_name, speed: @baud_rate, active: false)
-    Process.send_after(self(), :read_metrics, 5000)
+
+    :ok =
+      Circuits.UART.open(uart_pid, @port_name,
+        speed: @baud_rate,
+        active: false,
+        framing: CircuitsUARTFramingPMS7003
+      )
+
+    :timer.send_interval(@measurement_frequency, :read_metrics)
+
     {:ok, uart_pid}
   end
 
   @impl true
   def handle_info(:read_metrics, uart_pid) do
-    {:ok, message} = Circuits.UART.read(uart_pid, 5000)
+    {:ok, measurement} = Circuits.UART.read(uart_pid, @measurement_timeout)
 
-    case parse_message(message) do
-      {:ok, metrics} -> send_update(metrics)
-      {:error, error} -> Logger.info(error)
+    case measurement do
+      %CircuitsUARTFramingPMS7003.Measurement{} -> send_update(measurement)
+      _ -> Logger.info("ERROR - Received unknown measurement data: '#{inspect(measurement)}'")
     end
 
-    Process.send_after(self(), :read_metrics, 5000)
     {:noreply, uart_pid}
-  end
-
-  defp parse_message(message) do
-    with <<0x42, 0x4D, _padding::binary-size(8), data::binary-size(6), _rest::binary>> <- message,
-         <<pm1::binary-size(2), pm25::binary-size(2), pm10::binary-size(2)>> <- data do
-      metrics = %{
-        pm1: :binary.decode_unsigned(pm1),
-        pm25: :binary.decode_unsigned(pm25),
-        pm10: :binary.decode_unsigned(pm10)
-      }
-
-      {:ok, metrics}
-    else
-      _ -> {:error, :invalid_message}
-    end
   end
 
   defp send_update(metrics) do
